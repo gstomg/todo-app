5dafb8aa975e02bc6fa920bfadb42f12fb4d6eb5
Aelita Gizzatullina <amirkhanovaelita@gmail.com>
2025-09-27 20:22:46 +0500

    new

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -37,7 +37,7 @@ export function TodoItem({
       </button>
       <button onClick={() => 
         onDelete(todo.id)}>
-        <img src="/delete-icon.svg" alt="Удалить" />
+        <img src="./delete-icon.svg" alt="Удалить" />
       </button>
 
     </div>
