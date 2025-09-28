3b27aef4546450a0fcca558eb3b1aaf7747a55e0
Aelita Gizzatullina <amirkhanovaelita@gmail.com>
2025-09-28 11:46:32 +0500

    Update build files - 2025-09-28 11:46:32

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // для GitHub Pages
+  base: '/todo-app/', // для GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
